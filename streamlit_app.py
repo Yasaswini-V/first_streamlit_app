17e638bc543b6674dc75aa82d01eee23d9485ee5
Yasaswini-V <145005383+Yasaswini-V@users.noreply.github.com>
2024-01-09 10:46:59 +0530

    Update streamlit_app.py
    adding response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Health Diner') 
 streamlit.header('Breakfast Menu') 
@@ -15,3 +16,7 @@ fruits_selected=streamlit.multiselect("Pick some fruit: ",list(my_fruit_list.ind
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
